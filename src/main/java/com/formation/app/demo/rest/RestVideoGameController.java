d3ba1204dd010f3fd13bc391dac7b4dd4de2a911
charlymathieu <charly.mathieu.fr@gmail.com>
2019-07-04 16:07:18 +0200

    feat: rest get mapping added for picture

--- src/main/java/com/formation/app/demo/rest/RestVideoGameController.java
+++ src/main/java/com/formation/app/demo/rest/RestVideoGameController.java
@@ -1,8 +1,10 @@
 package com.formation.app.demo.rest;
 
 import com.formation.app.demo.entity.VideoGame;
+import com.formation.app.demo.service.StorageService;
 import com.formation.app.demo.service.VideoGameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,9 @@ public class RestVideoGameController {
 
     @Autowired
     private VideoGameService videoGameService;
+
+    @Autowired
+    private StorageService storageService;
 
 
     @GetMapping
@@ -32,6 +37,17 @@ public class RestVideoGameController {
             //TODO
             return ResponseEntity.notFound().build();
         }
+    }
+
+
+    @GetMapping(value = "/{id}/picture")
+    public ResponseEntity getPicture(@PathVariable("id") Long id) {
+        VideoGame videoGameToReturn = videoGameService.getSpecificGame(id).orElse(null);
+        String pictureName = videoGameToReturn.getPictureName();
+        Resource picture =  storageService.loadAsResource(pictureName);
+
+        return ResponseEntity.ok(picture);
+
     }
 
 
